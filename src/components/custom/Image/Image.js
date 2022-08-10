d380d77ca2785a118550a88c1c5ece98630c0693
Eugene Semenenko <fussy.gk@gmail.com>
2022-08-10 12:13:33 +0300

    Add Image and Text components

---
+++ src/components/custom/Image/Image.js
@@ -0,0 +1,38 @@
+import editForm from './Image.form';
+import field from '../../_classes/field/Field';
+
+export default class Image extends field {
+  static editForm = editForm;
+
+  static schema() {
+    return field.schema({
+      type: 'image',
+    });
+  }
+
+  static get builderInfo() {
+    return {
+      title: 'Изображение',
+      group: 'components',
+      icon: 'image',
+      weight: 7,
+      schema: Image.schema(),
+    };
+  }
+
+  constructor(...props) {
+    super(...props);
+  }
+
+  render() {
+    return super.render(`
+       ${this.component.image ? `<image class="image" src="${this.component.image}" /> ` : ''}
+    `);
+  }
+
+  attach(element) {
+    return super.attach(element);
+  }
+}
+
+// Components.addComponent("image", Image);
